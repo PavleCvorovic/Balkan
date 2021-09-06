4f4cbd20b17a8301a31a0e1be236787dca7bd20a
PavleCvorovic <80883401+PavleCvorovic@users.noreply.github.com>
2021-09-06 23:11:50 +0200

    b
    j

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,17 +16,10 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
 
 
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,
-        OPTIONS')
-            ->header('"Access-Control-Allow-Headers"', '*')
 
-            ->header('Access-Control-Allow-Origin', '*')
 
-            ->header('Access-Control-Allow-Origin', '*');
 
 
     }
