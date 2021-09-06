7a09d55a89dae7066e65c675e5db3c193546eb30
trifunovicnikola <nikola.trifunovic@ict-investment.me>
2021-09-06 15:09:48 +0200

    f

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,10 +14,12 @@ class Cors
      * @param \Closure $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
-
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,
+        OPTIONS')
+            ->header('"Access-Control-Allow-Headers"', '*');
     }
 }
