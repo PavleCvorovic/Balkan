5db71738abb03aae28399fc6e9cc10eb7b39f660
PavleCvorovic <80883401+PavleCvorovic@users.noreply.github.com>
2022-01-18 11:24:46 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RootController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -89,62 +91,62 @@ Route::get('getType_Razno/{tip}', 'raznoController@getType');
 
 
 //Admin api
-Route::post('addAutoTip', 'RootController@addAutomoto');
-Route::get('delAutoTip/{id}', 'rootController@delAutomoto');
-Route::get('getAutoTip', 'rootController@getAutomoto');
+Route::post('addAutoTip', [RootController::class,'addAutomoto']);
+Route::get('delAutoTip/{id}', [RootController::class,'delAutomoto']);
+Route::get('getAutoTip', [RootController::class,'getAutomoto']);
 
-Route::post('addHranaTip', 'rootController@addHrana');
-Route::get('delHranaTip/{id}', 'rootController@delHrana');
-Route::get('getHranaTip','rootController@getHrana');
+Route::post('addHranaTip', [RootController::class,'addHrana']);
+Route::get('delHranaTip/{id}', [RootController::class,'delHrana']);
+Route::get('getHranaTip',[RootController::class,'getHrana']);
 
-Route::post('addNekretnineTip', 'rootController@addNekretnine');
-Route::get('delNekretnineTip/{id}', 'rootController@delNekretnine');
-Route::get('getNekretnineTip','rootController@getNekretnine');
+Route::post('addNekretnineTip', [RootController::class,'addNekretnine']);
+Route::get('delNekretnineTip/{id}', [RootController::class,'delNekretnine']);
+Route::get('getNekretnineTip',[RootController::class,'getNekretnine']);
 
-Route::post('addOdjecaTip', 'rootController@addOdjeca');
-Route::get('delOdjecaTip/{id}', 'rootController@delOdjeca');
-Route::get('getOdjecaTip','rootController@getOdjeca');
+Route::post('addOdjecaTip', [RootController::class,'addPosao']);
+Route::get('delOdjecaTip/{id}', [RootController::class,'delOdjeca']);
+Route::get('getOdjecaTip',[RootController::class,'getOdjeca']);
 
-Route::post('addPosaoTip', 'rootController@addPosao');
-Route::get('delPosaoTip/{id}', 'rootController@delPosao');
-Route::get('getPosaoTip','rootController@getPosao');
+Route::post('addPosaoTip', [RootController::class,'addPosao']);
+Route::get('delPosaoTip/{id}', [RootController::class,'delPosao']);
+Route::get('getPosaoTip',[RootController::class,'getRazno']);
 
-Route::post('addRaznoTip', 'rootController@addRazno');
-Route::get('delRaznoTip/{id}', 'rootController@delRazno');
-Route::get('getRaznoTip','rootController@getRazno');
+Route::post('addRaznoTip', [RootController::class,'addRazno']);
+Route::get('delRaznoTip/{id}', [RootController::class,'delRazno']);
+Route::get('getRaznoTip',[RootController::class,'getRazno']);
 
-Route::post('addTehnikaTip', 'rootController@addTehnika');
-Route::get('delTehnikaTip/{id}', 'rootController@delTehnika');
-Route::get('getTehnikaTip','rootController@getTehnika');
+Route::post('addTehnikaTip', [RootController::class,'addTehnika']);
+Route::get('delTehnikaTip/{id}', [RootController::class,'delTehnika']);
+Route::get('getTehnikaTip',[RootController::class,'getTehnika']);
 
-Route::post('register','UserController@register');
-Route::post('login','UserController@login');
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
 
 //Saljes id korisnika
-Route::get('GetPostByUser/{id}', 'UserController@getPostbyUser');
+Route::get('GetPostByUser/{id}', [UserController::class,'getPostbyIdUser']);
 
 // Saljes id produkta,id korisnika i naziv tabele.
-Route::post('delAsUser', 'UserController@DelAsUser');
+Route::post('delAsUser', [UserController::class,'DelAsUser']);
 //Saljes id produkta, ostale parametre koji pripadaju odredjenom produktu i naziv tabele.
-Route::post('modAsUser', 'UserController@ModAsUser');
+Route::post('modAsUser', [UserController::class,'ModAsUser']);
 //Saljes parametre ,naziv tabele i id vrste tabele
 
 
-Route::post('getAllId', 'UserController@GetId');
-Route::post('filter', 'UserController@Filter');
-Route::post('getPostsAll', 'UserController@GetPosts');
-Route::get('adminGet', 'UserController@deleteAdmin');
-Route::get('getAllFeatured', 'UserController@getAllFeatured');
-Route::get('getAllRandom', 'UserController@getAllRandom');
-Route::get('getAllNew', 'UserController@getAllNew');
-Route::get('getAllCheap', 'UserController@getAllCheap');
-Route::get('getNewest', 'UserController@getNewest');
-Route::get('showUsers', 'UserController@show');
-Route::get('showUser/{id}', 'UserController@showId');
-Route::delete('deleteUser/{id}', 'UserController@delete');
-Route::post('setAllNew', 'UserController@setAllNew');
-Route::post('deleteNew', 'UserController@deleteNew');
+Route::post('getAllId', [UserController::class,'GetId']);
+Route::post('filter', [UserController::class,'Filter']);
+Route::post('getPostsAll', [UserController::class,'GetPosts']);
+Route::get('adminGet', [UserController::class,'deleteAdmin']);
+Route::get('getAllFeatured', [UserController::class,'getAllFeatured']);
+Route::get('getAllRandom', [UserController::class,'getAllRandom']);
+Route::get('getAllNew', [UserController::class,'getAllNew']);
+Route::get('getAllCheap', [UserController::class,'getAllCheap']);
+Route::get('getNewest', [UserController::class,'getNewest']);
+Route::get('showUsers', [UserController::class,'show']);
+Route::get('showUser/{id}', [UserController::class,'showId']);
+Route::delete('deleteUser/{id}', [UserController::class,'delete']);
+Route::post('setAllNew', [UserController::class,'setAllNew']);
+Route::post('deleteNew', [UserController::class,'deleteNew']);
 
-Route::post('modPostByUser', 'UserController@modPostUser');
+Route::post('modPostByUser', [UserController::class,'modPostUser']);
 
-Route::post('getUserPostId', 'UserController@getPostbyIdUser');
+Route::post('getUserPostId', [UserController::class,'getPostbyIdUser']);
